f0c156f2cfc7fbf36485c462edafbe0ab50584b0
Eduardo Nogueira Santos <87313525+edunogueirasantos@users.noreply.github.com>
2022-12-28 13:15:43 -0300

    Add files via upload

---
+++ src/container/Intro/Intro.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { BsFillPlayFill, BsPauseFill } from 'react';
+
+import { meal } from '../../constants';
+import './Intro.css';
+
+const Intro = () => {
+  const [playVideo, setPlayVideo] = React.useState(false);
+  const vidRef = React.useRef();
+
+  return(
+    <div className='app__video'>
+      <video 
+        src={meal} 
+        ref={vidRef}
+        type="video/mp4"
+        loop
+        controls={false}
+        muted
+      />
+      <div className='app__video-overlay_circle flex__center'>
+        <div 
+          className='app__video-overlay_circle flex__center'
+          onClick={() => {
+            setPlayVideo(!playVideo);
+            if (playVideo) {
+              vidRef.current.pause();
+            } else {
+              vidRef.current.play();
+            }
+          }}
+        >
+          {playVideo ? (
+            <BsPauseFill color="#white" fontSize={30} />
+          ) : (
+            <BsFillPlayFill color="white" fontSize={30} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+export default Intro;
